8877b8ca37181b886250a677aaccc35b98701466
DTT26 <tungdtde180564@fpt.edu.vn>
2026-06-22 10:04:51 +0700

    Fix MongoParseError by removing deprecated options

--- db.js
+++ db.js
@@ -3,8 +3,5 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log('Connected to MongoDB'))
+mongoose.connect(process.env.MONGO_URI).then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('Error connecting to MongoDB', err));
